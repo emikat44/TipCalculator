6cc9989169394cc81329ff1c3a3a3ee8059dcd51
emikat44 <103278996+emikat44@users.noreply.github.com>
2022-11-09 17:23:28 +0000

    Deploying to gh-pages from  @ 3530300f5acd8f7b6dcdbc621b9dd76b377e06b4 🚀

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -245,11 +245,19 @@
       "url": "favicon.ico"
     },
     {
+      "hash": "sha256-DbpQaq68ZSb5IoPosBErM1QWBfsbTxpJqhU0REi6wP4=",
+      "url": "icon-192.png"
+    },
+    {
+      "hash": "sha256-oEo6d+KqX5fjxTiZk\/w9NB3Mi0+ycS5yLwCKwr4IkbA=",
+      "url": "icon-512.png"
+    },
+    {
       "hash": "sha256-mpicjmyACYXnftzGGbra2jpy7q+VVpr7kQQlFAk0gJY=",
       "url": "index.html"
     },
     {
-      "hash": "sha256-7LubsO\/RtLHpoa0gbEMgRuuYcjt1b\/vIu\/Obnu5uKSs=",
+      "hash": "sha256-pRS45oOrD\/KR\/eVDhX9ruQeG7aZ+ydnfIJ9hua7ESN0=",
       "url": "manifest.json"
     },
     {
@@ -269,5 +277,5 @@
       "url": "Waffle512.png"
     }
   ],
-  "version": "DVdzGPxo"
+  "version": "JlTasdLR"
 };
